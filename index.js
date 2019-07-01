6bd17a9deb1e0649769b697ed36a4bc30a5ad02a
lydiathornton <lydia.d.thornton@gmail.com>
2019-06-30 17:16:18 -0700

    updated with responsive design and new projects

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 class tabLink {
-    constructor(element) {
-      this.element = element;
-      console.log(this.element)
-      this.tabData = this.element.dataset.tab;
-      if (this.tabData === 'all') {
-        this.cards = document.querySelectorAll('.card');
-      } else {
-        this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`)
-      }
-      this.cards = Array.from(this.cards).map(card => new TabCard(card));
-      this.element.addEventListener('click', (event) => this.selectTab(event))
+  constructor(element) {
+    this.element = element;
+    console.log(this.element);
+    this.tabData = this.element.dataset.tab;
+    if (this.tabData === "all") {
+      this.cards = document.querySelectorAll(".card");
+    } else {
+      this.cards = document.querySelectorAll(
+        `.card[data-tab='${this.tabData}']`
+      );
     }
-    
-    selectTab() {
-      const tabs = document.querySelectorAll('.tab');
-      tabs.forEach(tab => tab.classList.remove('active-tab'));
-      const cards = document.querySelectorAll('.card');
-      cards.forEach(card => card.style.display = 'none');
-      this.element.classList.add('active-tab');
-      this.cards.forEach(card => card.selectCard(this.TabCard));
-      
-    }
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
+    this.element.addEventListener("click", event => this.selectTab(event));
   }
-  
-  class TabCard {
-    constructor(cardElement) {
-      this.cardElement = cardElement;
-    }
-    selectCard() {
-      this.cardElement.style.display = "flex"
-    }
+
+  selectTab() {
+    const tabs = document.querySelectorAll(".tab");
+    tabs.forEach(tab => tab.classList.remove("active-tab"));
+    const cards = document.querySelectorAll(".card");
+    cards.forEach(card => (card.style.display = "none"));
+    this.element.classList.add("active-tab");
+    this.cards.forEach(card => card.selectCard(this.TabCard));
   }
-  
-  let tabs = document.querySelectorAll('.tab');
-  tabs.forEach(tab => new tabLink(tab));
+}
+
+class TabCard {
+  constructor(cardElement) {
+    this.cardElement = cardElement;
+  }
+  selectCard() {
+    this.cardElement.style.display = "flex";
+  }
+}
+
+let tabs = document.querySelectorAll(".tab");
+tabs.forEach(tab => new tabLink(tab));
